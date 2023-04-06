d0943c52c5c0019548bbc52d0458e5311af5def2
unmeimusu <unmeimusu@gmail.com>
2023-04-06 23:49:17 +0700

    Update file

---
+++ app.js
@@ -0,0 +1,10 @@
+// an object
+var person = {
+    name: "John", age: 31, favColor: "green", height: 183
+}
+// call object
+console.log(person)
+// call property of object
+console.log(person.favColor + " same as " + person["favColor"])
+// length of characters number in a property
+document.write(person.name.length)
