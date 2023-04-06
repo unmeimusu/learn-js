bcd75cec846d3790c3b2756111a56b0e0acd47ca
unmeimusu <unmeimusu@gmail.com>
2023-04-07 05:30:01 +0700

    Update file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// an object
+// an object initializer
 var person1 = {
     name: "John", age: 31, favColor: "green", height: 183
 }
@@ -9,7 +9,7 @@ console.log(person1.favColor + " same as " + person1["favColor"])
 // length of characters number in a property
 document.write(person1.name.length) // document is an object and write() is a method of the document object
 
-
+// constructor function
 function person(name, age) {
     this.name = name;  
     this.age = age;
@@ -17,8 +17,10 @@ function person(name, age) {
         this.name = name;
     }
 }
-
-var p = new person("David", 21);
-p.changeName("John");
-
-document.write(p.name);
+// creating an instance of an object
+var p1 = new person("David", 21);
+var p2 = new person("Monty",22)
+p1.changeName("John");
+// output to the dom
+document.write(p1.name);
+document.write(p2.name);
