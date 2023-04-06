981f6b4413fba7789243ed9b190b83ef4e58a00a
unmeimusu <unmeimusu@gmail.com>
2023-04-07 05:14:01 +0700

    Update file

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 // an object
-var person = {
+var person1 = {
     name: "John", age: 31, favColor: "green", height: 183
 }
 // call object
-console.log(person)
+console.log(person1)
 // call property of object
-console.log(person.favColor + " same as " + person["favColor"])
+console.log(person1.favColor + " same as " + person1["favColor"])
 // length of characters number in a property
-document.write(person.name.length) // document is an object and write() is a method of the document object
+document.write(person1.name.length) // document is an object and write() is a method of the document object
+
+
+function person(name, age) {
+    this.name = name;  
+    this.age = age;
+    this.changeName = function (name) {
+        this.name = name;
+    }
+}
+
+var p = new person("David", 21);
+p.changeName("John");
+
+document.write(p.name);
