d3403f18facd1c851453d6b3053de8ccbdf78ea2
unmeimusu <unmeimusu@gmail.com>
2023-04-08 03:41:44 +0700

    Update file

--- app.js
+++ app.js
@@ -24,3 +24,27 @@ p1.changeName("John");
 // output to the dom
 document.write(p1.name);
 document.write(p2.name);
+// Method to calculate discount price
+function main() {
+    var prodID = 112
+    var price = parseInt(165000,10)
+    var discount = parseInt(5000,10)
+
+    var prod1 = new Product(prodID, price)
+    console.log(prod1.prodID + " price: " + prod1,price)
+    
+    prod1.changePrice(discount)
+    console.log(prod1.prodID + " new price: " + prod1.price)
+    
+}
+
+function Product(prodID, price) {
+    this.prodID = prodID
+    this.price = price
+
+    this.changePrice = function (discount) {
+        this.price = this.price - (discount/100 * this.price)
+    }
+}
+
+main()
