d0d1f25ab53073994e1f8892aac3aeaf74848d54
unmeimusu <unmeimusu@gmail.com>
2023-04-15 17:25:35 +0700

    Update file

--- app.js
+++ app.js
@@ -616,7 +616,7 @@ console.log("h: ", h112, ", s: ", s112)
 let a12,b12
 ({a12,b12} = {a12: "Hello",b12:"World"})
 console.log("a12: ",a12," b12: ",b12)
-// Destructuring data
+// Destructuring const object
 const obj44 = {one: 1, two: 2}
 let {one: first, two: second} = obj44
 console.log(one)
