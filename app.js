bc69e80bd618f2ba7770f78505d450b99b403fb7
unmeimusu <unmeimusu@gmail.com>
2023-04-18 06:27:18 +0700

    Update file

--- app.js
+++ app.js
@@ -872,3 +872,28 @@ function asyncFunc(work) {
     })
 }
 // Async Await : way better + try catch error
+function asyncFunc(work) {
+    return new Promise(function(resolve,reject) {
+        if (work === "")
+            reject(Error("Nothing"))
+        setTimeout(function() {
+            resolve()
+        })
+    })
+}
+
+asyncFunc("Work 1")
+.then(function(result) {
+    console.log(result)
+    return asyncFunc("Work 2")
+}, function(error) {
+    console.log(error)
+})
+.then(function(result) {
+    console.log(result)
+}, function(result) {
+    console.log(result)
+}, function (error) {
+    console.log(error)
+});
+console.log("End")
